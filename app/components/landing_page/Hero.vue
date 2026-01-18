641b26c210f5f8b60abc2c657373dcfd614f63c5
Fry <fryadoumorrel@gmail.com>
2026-01-18 01:40:14 +0100

    Image page 1 landing page

---
+++ app/components/landing_page/Hero.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="relative w-full h-[1000px] overflow-hidden"> 
+  <img 
+      src="/images/Rectangle-160.png" 
+      class="absolute 
+             left-[680px] 
+             top-[441px] 
+             w-[270px] 
+             h-[431px] 
+             rounded-[35px] 
+             
+             shadow-lg"
+      alt="Carte Grand Frère"
+    />
+  </div>
+
+   <div class="relative w-full h-[1000px] overflow-hidden"> <img 
+      src="/images/Rectangle-140.png" 
+      class="absolute 
+             left-[680px] 
+             top-[441px] 
+             w-[270px] 
+             h-[431px] 
+             rounded-[35px] 
+             
+             shadow-lg"
+      alt="Carte Grand Frère"
+    />
+
+
+    <div class="min-h-screen bg-gradient-to-br from-[#FAF7F2] via-[#fdf2f2] to-[#fff4e6]">
+  </div>
+
+
+
+   
+       <button class="absolute left-[795px] top-[364px] bg-gradient-to-r from-[#e67e22] to-[#a55eea] px-[10px] py-[20px] rounded-[12px] text-white font-bold shadow-md hover:opacity-90 transition">
+        Commencez maintenant
+      </button>
+
+
+       <button class="absolute left-[495px] top-[364px]   px-[10px] py-[20px]  text-[#333333] rounded-[10px] font-medium border border-gray-300">
+        Chargez plus d'activités
+      </button>
+
+  </div>
+</template>
